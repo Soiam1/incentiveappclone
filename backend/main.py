4cb78587a65f70ec07fbb5646662f28bb8aa5953
Atm-tech <advancedtradingmart@gmail.com>
2025-06-18 16:49:28 +0530

    add url

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,7 @@ app = FastAPI(title="Incentive Management System")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://incentive-app-eta.vercel.app","https://incentive-backend-kgpj.onrender.com"],
+    allow_origins=["http://localhost:5173", "https://incentive-app-eta.vercel.app","https://incentive-app-gf1z.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
