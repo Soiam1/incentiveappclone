6cb27d206f129deaabbad951a43cefd497a78c10
Soiam1 <saad.apoorti@gmail.com>
2025-06-23 13:34:55 +0530

    for barcode scan build

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,7 @@ app = FastAPI(title="Incentive Management System")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://incentive-app-vert.vercel.app","https://institution-acknowledge-neighbor-peru.trycloudflare.com"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
